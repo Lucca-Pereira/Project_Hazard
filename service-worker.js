20b760c3c92bfa4315bc0f51f6da3243e42eabc3
Lucca Pereira <67197943+Lucca-Pereira@users.noreply.github.com>
2025-09-25 19:38:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,20 @@
-const CACHE_NAME = "hazard-map-cache";
-const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "service-worker.js",
-  "icon-192.png",
-  "icon-512.png",
-  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
-  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-];
-
-// Install: cache files
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open("hazardmap-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./manifest.json"
+      ]);
+    })
   );
-  self.skipWaiting();
 });
 
-// Activate: clear old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch: network first, fallback to cache
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, clone);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });

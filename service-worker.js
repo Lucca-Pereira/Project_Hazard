415e50e31c6e4714767c12dd2664c9fb7b3abf53
Lucca Pereira <67197943+Lucca-Pereira@users.noreply.github.com>
2025-09-25 13:29:14 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "hazard-map-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

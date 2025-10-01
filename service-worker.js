fe39f8e3bb0c5af317fdb2a0b8357270daea7f38
lucperhea <lucperhea@alum.us.es>
2025-10-01 17:34:27 +0200

    Help

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,28 @@
+const CACHE_NAME = "hazardmap-cache-v3";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./service-worker.js",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("hazardmap-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
